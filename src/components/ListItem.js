0c6b8688a57beabf93a62f84558d5c2dca24bd8b
KonstantinDinev <konstDinev@gmail.com>
2019-07-03 14:28:54 +0300

    Add selection reducer and ListItem expand

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common/';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    if (library.item.id === selectedLibraryId) {
+      return (
+        <Text>{library.item.description}</Text>
+      );
+    }
+  }
 
   render() {
     //console.log(this.props);
 
+    const { titleSize } = styles;
+    const { id, title } = this.props.library.item;
+
     return (
-      <CardSection>
-        <Text>{this.props.library.item.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleSize}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
-export default ListItem;
+const styles = {
+  titleSize: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
---
+++ src/reducers/SelectionReducer.js
@@ -0,0 +1,10 @@
+export default (state = null, action) => {
+  console.log(action, state);
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
